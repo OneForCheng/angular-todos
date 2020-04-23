7701d80440bc1ee3b1af0055a6f58f4d81f26100
forcheng <2871979271@qq.com>
2020-04-23 18:00:38 +0800

    forcheng - enable `REDUX_DEVTOOLS_EXTENSION`

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,10 @@
 import { createStore } from 'redux';
 import reducers from './reducers/index';
 
-export default createStore(reducers);
+const win: any = window as any;
+
+export default createStore(
+  reducers,
+  undefined,
+  win.__REDUX_DEVTOOLS_EXTENSION__ && win.__REDUX_DEVTOOLS_EXTENSION__()
+);
