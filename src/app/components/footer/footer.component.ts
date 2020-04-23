00dcbaed39f9f3831071facb7e8ca0b7034aad05
forcheng <2871979271@qq.com>
2020-04-23 21:41:26 +0800

    forcheng - extract `updateState` to init state in `ngOnInit` of `FooterComponent`

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -19,16 +19,19 @@ export class FooterComponent implements OnInit, OnDestroy {
   private readonly unsubscribe: any;
 
   constructor(private todosService: TodosService, private filtersService: FiltersService) {
-    this.unsubscribe = store.subscribe(() => {
-      const state = store.getState();
-      const todos = state.todos;
-      this.filters = state.filters;
-      this.activeCount = todos.filter(todo => !todo.completed).length;
-      this.isShowClearBtn = todos.some(todo => todo.completed);
-    });
+    this.unsubscribe = store.subscribe(this.updateState);
+  }
+
+  private updateState = () => {
+    const state = store.getState();
+    const todos = state.todos;
+    this.filters = state.filters;
+    this.activeCount = todos.filter(todo => !todo.completed).length;
+    this.isShowClearBtn = todos.some(todo => todo.completed);
   }
 
   ngOnInit() {
+    this.updateState();
   }
 
   switchFilter = (label: string) => {
